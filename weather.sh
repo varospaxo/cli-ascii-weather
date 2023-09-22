c35ce41b709c116283a323fad22b1772cb0f00a4
varospaxo <64273563+varospaxo@users.noreply.github.com>
2023-09-23 01:23:15 +0530

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,9 +1,8 @@
-'''basic'''
-sudo apt install figlet -y
-sudo apt install lolcat -y
-clear
-read -p "Enter city: " city
-read -p "Days required (0, 1, 2, 3): " columns
-clear
-figlet "Weather in '${city}'" | lolcat
-curl "wttr.in/${city}?${columns}"
+sudo apt install figlet -y;
+sudo apt install lolcat -y;
+clear;
+read -p "Enter city: " city;
+read -p "Days required (0, 1, 2, 3): " columns;
+clear;
+figlet "Weather in '${city}'" | lolcat;
+curl "wttr.in/${city}?${columns}";
